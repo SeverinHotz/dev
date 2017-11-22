91b2a3080db345e5c13a31c938faea3a71a10548
Severin Hotz <Severin_Hotz@rcomext.com>
2017-11-22 11:01:42 +0100

    Checked in EXO connect

--- Source/Public/New-EXOConnection.ps1
+++ Source/Public/New-EXOConnection.ps1
@@ -5,49 +5,62 @@
     .Description
     Cmdlet to connect to exchange online with modern authentication and proxy settings.
     Proxy settings configured in IE are used by default.
-    The connection is stored in a variable exchangeOnlineSession for later use.
+    Please remove your connection before closing the Powershell session.
+    Remove-PSSession $ExchangeOnlineSession
     
     .Example
     # Connect to exchange online
-    New-EXOConnection
+    New-SREXOConnection
+
+    .Example
+    # Conenct to exchange online using a username
+    New-SREXOConnection -UserPrincipalName prename_lastname@domain.com
 
     .Example
     # Connect to exchange online without using proxy settings.
-    New-EXOConnection -NoProxy
+    New-SREXOConnection -NoProxy
+
+    .Example
+    # Please remove your connection before closing
+    Remove-PSSession $ExchangeOnlineSession
 
 #>
 
-function New-EXOConnection {
+function New-SREXOConnection {
 
      [CmdletBinding()]
     
          param (
-            [switch] $NoProxy    
+            # Parameter to switch off proxy configuration
+            [switch] $NoProxy,    
+            # User Principal Name or email address of the user
+            [string] $UserPrincipalName = ''
         )
     
-        begin {
-
-            # Fetch the path of the exchange online module
-            $ModuleName = $PSModuleRoot + "\Private\Microsoft.Exchange.Management.ExoPowershellModule.dll"
-            # Import the module
-            Import-Module -FullyQualifiedName $ModuleName -Force
-            # Create connection to exchange online
-            if($NoProxy)
-            {
-                # connect without proxy server settings
-                $null = import-module (Connect-EXOPSSession) -global -DisableNameChecking
-            }
-            else
-            {
-                # Fetch proxy settings from internet explorer and connect
-                $proxysettings = New-PSSessionOption -ProxyAccessType IEConfig
-                $null = import-module (Connect-EXOPSSession -PSSessionOption $proxysettings) -global -DisableNameChecking
-            }    
-            
+        begin {           
         }  
         
         process {
-        
+
+            # Fetch proxy settings from internet explorer and connect
+            if (!$NoProxy)
+            {
+                # Verbose output regarding proxy settings
+                write-verbose "Proxy seetting from internet explorer are used"
+                $proxysettings = New-PSSessionOption -ProxyAccessType IEConfig
+            }
+            else
+            {
+                # Verbose output regarding proxy settings
+                write-verbose "No Proxy seetting are used"
+            }
+                        
+            # Create connection to exchange online        
+            $null = import-module (Connect-EXOPSSession -PSSessionOption $proxysettings -Userprincipalname $UserPrincipalName) -global -DisableNameChecking
+
+            # Store powershell session in a variable for later use
+            $ExchangeOnlineSession = (Get-PSSession | Where-Object { ($_.ConfigurationName -eq 'Microsoft.Exchange') -and ($_.State -eq 'Opened') })[0]
+                
         }
         
         end {
